5fc266a5076483b4fab189e913d7f926d05c2708
michelange2008 <michelange@wanadoo.fr>
2019-11-06 18:50:05 +0100

    demande seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,19 +11,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-      $this->call(UsertypesTableSeeder::class);
-      $this->call(UsersTableSeeder::class);
-      $this->call(IconesTableSeeder::class);
-      $this->call(QttsTableSeeder::class);
-      $this->call(VetosTableSeeder::class);
-      $this->call(UnitesTableSeeder::class);
-      $this->call(EspecesTableSeeder::class);
-      $this->call(ClientsTableSeeder::class);
-      $this->call(AdminsTableSeeder::class);
-      $this->call(AnalistesTableSeeder::class);
-      $this->call(AnaitemsTableSeeder::class);
-      $this->call(AnaactesTableSeeder::class);
-      $this->call(AnapacksTableSeeder::class);
-      // $this->call(DemandeTableSeeder::class);
+      // $this->call(UsertypesTableSeeder::class);
+      // $this->call(UsersTableSeeder::class);
+      // $this->call(IconesTableSeeder::class);
+      // $this->call(QttsTableSeeder::class);
+      // $this->call(VetosTableSeeder::class);
+      // $this->call(UnitesTableSeeder::class);
+      // $this->call(EspecesTableSeeder::class);
+      // $this->call(ClientsTableSeeder::class);
+      // $this->call(AdminsTableSeeder::class);
+      // $this->call(AnalistesTableSeeder::class);
+      // $this->call(AnaitemsTableSeeder::class);
+      // $this->call(AnaactesTableSeeder::class);
+      // $this->call(AnapacksTableSeeder::class);
+      $this->call(DemandeTableSeeder::class);
     }
 }
