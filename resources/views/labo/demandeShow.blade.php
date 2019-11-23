5e13965174fdb2b6ace1c28fa779f594bb9b6179
Michel BOUY <michelange@wanadoo.fr>
2019-11-23 13:21:39 +0100

    petit oubli

--- resources/views/labo/demandeShow.blade.php
+++ resources/views/labo/demandeShow.blade.php
@@ -14,6 +14,11 @@
         ])
       </div>
     </div>
+    <div class="row">
+      <div class="col-md-8">
+        @include('fragments.demandeDetail' ['demande' => $demande])
+      </div>
+    </div>
   </div>
 
 @endsection
