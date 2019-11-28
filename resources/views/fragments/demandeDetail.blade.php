e9ddc75cc93a48df04f2b92f6ad8c445f33b303d
Michel BOUY <michelange@wanadoo.fr>
2019-11-28 06:54:00 +0100

    suite demande show

--- resources/views/fragments/demandeDetail.blade.php
+++ resources/views/fragments/demandeDetail.blade.php
@@ -1,81 +1,173 @@
-<div class="collapse" id="{{ $collapse }}">
+<div class="" id="{{ $collapse }}">
   <div class="card card-body">
     <div class="row">
       <div class="col-md-12 d-inline-flex d-flex justify-content-between">
         <img class="img-50" src="{{ asset('/storage/img/icones/')."/".$demande->espece->icone->nom }}" alt="espece">
-        <h5 class="card-title">
-          <a href="{{ route('eleveurAdmin.show', $demande->user->id) }}">
-            {{ $demande->user->name }}
+        <h3 class="card-title">
+          <a class=" color-rouge-tres-fonce" href="{{ route('eleveurAdmin.show', $demande->user->id) }}">
+            <strong>{{ $demande->user->name }}</strong>
           </a>
+        </h3>
+        <h5>
+          ({{ $demande->user->eleveur->tel}})
         </h5>
-        <p> ({{ $demande->user->eleveur->tel}}) </p>
       </div>
     </div>
-    @if ($demande->toveto)
-      <div class="row my-3">
-        <div class="col-md-12">
-          <small>Vétérinaire:</small>
-          <a href="{{ route('vetoAdmin.show', $demande->veto_id)}}">
-            <strong>{{ $demande->veto->user->name}}</strong>
-          </a>
-        </div>
-      </div>
-    @endif
     <div class="row my-3">
-      <div class="col-md-6">
-          <small>Date de prélèvement</small>
-          @include('fragments.colonneDate', ['date' => $demande->date_prelevement])
-          <small>Date de réception</small>
-          @include('fragments.colonneDate', ['date' => $demande->date_reception])
-          <small>Date de resultats</small>
-          @include('fragments.colonneDate', ['date' => $demande->date_resultats])
-          <small>Date d'envoi</small>
-          @include('fragments.colonneDate', ['date' => $demande->date_envoi])
 
+<!-- AFFICHAGE DES INFORMATIONS RELATIVES A L'ANALYSE -->
+
+      <div class="col-md-6">
+        <div class="row mx-1">
+          <div class="col-md-12 alert-bleu-tres-fonce pt-3 d-inline-flex justify-content-around">
+            <h5 class="card-title">ANALYSE</h5>
+            <a href="#" title="voir">
+              <i class="color-rouge-tres-clair material-icons">picture_as_pdf</i>
+            </a>
+          </div>
+        </div>
+        <div class="row mx-1 border">
+          <table class="table table-hover">
+            <tbody>
+              @if ($demande->toveto)
+                <tr>
+                  <td>
+                    <small>-> Vétérinaire</small>
+                  </td>
+                  <td>
+                    <a href="{{ route('vetoAdmin.show', $demande->veto_id)}}">
+                      <strong>{{ $demande->veto->user->name}}</strong>
+                    </a>
+                  </td>
+                </tr>
+              @endif
+
+              <tr>
+                <td>
+                  <small>Date de prélèvement : </small>
+                </td>
+                <td>
+                  @if ($demande->date_prelevement !== null)
+                    @include('fragments.colonneDate', ['date' => $demande->date_prelevement])
+                  @else
+                    NC
+                  @endif
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <small>Date de réception :</small>
+                </td>
+                <td>
+                  @include('fragments.colonneDate', ['date' => $demande->date_reception])
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <small>Date de resultats : </small>
+                </td>
+                <td>
+                  @include('fragments.colonneDate', ['date' => $demande->date_resultats])
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <small>Date d'envoi : </small>
+                </td>
+                <td>
+                  @include('fragments.colonneDate', ['date' => $demande->date_envoi])
+                </td>
+              </tr>
+              <tr>
+                <td>{{ $demande->user->name }}</td>
+                <td>
+                  <a href="#" title="Renvoyer l'email à l'éleveur">
+                    <i class="material-icons">play_arrow</i> <i class="material-icons">email</i></td>
+                  </a>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
+
+<!-- AFFUCHAGE DES INFORMATIONS RELATIVES À LA FACTURE-->
+
+      <div class="col-md-6">
+        <div class="row mx-1">
+          <div class="col-md-12 alert-bleu-tres-fonce pt-3 d-inline-flex justify-content-around">
+            <h5 class="card-title">FACTURE</h5>
+            <a href="#" title="voir">
+              <i class="color-rouge-tres-clair material-icons">picture_as_pdf</i>
+            </a>
+          </div>
+        </div>
+        <div class="row mx-1 border">
+          <div class="col-md-12">
+            <table class="table table-hover">
+              <tbody>
+                <tr>
+                  <td>
+                    <small>Destinataire : </small>
+                  </td>
+                  <td>
+                    {{ $demande->facture->user->name}}
+                  </td>
+                </tr>
+                <tr>
+                  @if ($demande->facture->faite)
+                    <td>
+                      <small>Montant TTC : </small>
+                    </td>
+                    <td>
+                      <strong>{{ $demande->facture->total_ttc}} € TTC</strong>
+                    </td>
+                  @else
+                    <td class="color-rouge-tres-fonce"><i class="material-icons">warning</i></td>
+                    <td class="color-rouge-tres-fonce">
+                      facture non établie
+                    </td>
+                  @endif
+                </tr>
+                @if ($demande->facture->envoyee)
+                  <tr>
+                    <td>
+                      <small>Facture envoyée le </small>
+                    </td>
+                    <td>
+                      @include('fragments.colonneDate', ['date' => $demande->facture->envoyee_date])
+                    </td>
+                  </tr>
+                @elseif ($demande->facture->faite)
+                  <tr>
+                    <td class="color-rouge-tres-fonce"><i class="material-icons">warning</i></td>
+                    <td class="color-rouge-tres-fonce">
+                      facture non envoyée
+                    </td>
+                  </tr>
+                @endif
+                @if ($demande->facture->payee)
+                  <tr class="alert-success">
+                    <td>
+                    <small>Facture payée le </small>
+                    </td>
+                    <td>
+                      @include('fragments.colonneDate', ['date' => $demande->facture->payee_date])
+                    </td>
+                  </tr>
+                @elseif ($demande->facture->envoyee)
+                  <tr>
+                    <td class="color-rouge-tres-fonce"><i class="material-icons">warning</i></td>
+                    <td class="color-rouge-tres-fonce">
+                      facture non payée
+                    </td>
+                  </tr>
+                @endif
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="row my-3">
-      <h5>Facture</h5>
-    </div>
-    <div class="row">
-      <div class="col-md-4">
-        <small>Destinataire : </small>{{ $demande->facture->user->name}}
-      </div>
-      @if ($demande->facture->faite)
-        <div class="col-md-4">
-          <small>Montant HT : </small>{{ $demande->facture->total_ht}} €
-        </div>
-        <div class="col-md-4">
-          <small>Montant TTC : </small>{{ $demande->facture->total_ht}} €
-        </div>
-      @else
-        <div class="col-md-8 alert-rouge-tres-fonce">
-          facture non établie
-        </div>
-      @endif
-    </div>
-    <div class="row my-3">
-      @if ($demande->facture->envoyee)
-        <div class="col-md-6">
-          <small>Facture envoyée le </small>
-          @include('fragments.colonneDate', ['date' => $demande->facture->envoyee_date])
-        </div>
-      @elseif ($demande->facture->faite)
-        <div class="col-md-6 alert-rouge-tres-fonce">
-          facture non envoyée
-        </div>
-      @endif
-      @if ($demande->facture->payee)
-        <div class="col-md-6 alert-success">
-          <small>Facture payée le </small>
-          @include('fragments.colonneDate', ['date' => $demande->facture->payee_date])
-        </div>
-      @elseif ($demande->facture->envoyee)
-        <div class="col-md-6 alert-rouge-tres-fonce">
-          facture non payée
-        </div>
-      @endif
-    </div>
-  </div>
+
+
 </div>
