4229b1c8588e19916bb7021e1c7c098b0a9015a1
michelange2008 <michelange@wanadoo.fr>
2020-01-30 17:58:31 +0100

    Debut affichage véto

--- app/Http/Controllers/VeterinaireController.php
+++ app/Http/Controllers/VeterinaireController.php
@@ -4,17 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Traits\LitJson;
+use App\Http\Traits\DemandeFactory;
+
+use App\Models\Productions\Demande;
+
 class VeterinaireController extends Controller
 {
+
+  use LitJson, DemandeFactory;
+
+  protected $menu;
+
     public function __construct()
     {
-        $this->middleware('auth');
-        $this->middleware('veto');
+      $this->menu = $this->litJson('menuExtranet');
+
+      $this->middleware('auth');
+      $this->middleware('veto');
+
     }
 
     public function index()
     {
-      return "vétérinaire";
+      $demande = Demande::where('veto_id', auth()->user())
+
+      return view('Utilisateurs.vetos.indexVeto', [
+        'menu' => $this->menu,
+        'demande' => $demande,
+      ]);
+
     }
 }
 // TODO: Comment faire avec le "aucun vétérinaire"
---
+++ resources/views/utilisateurs/vetos/indexVeto.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@extends('extranet.menuExtranet')
+
+@section('content')
+
+  <div class="container-fluid">
+
+    <div class="row my-3 justify-content-center">
+
+      <div class="col-md-10 col-lg-8">
+
+
+
+      </div>
+
+    </div>
+
+  </div>
+
+@endsection
