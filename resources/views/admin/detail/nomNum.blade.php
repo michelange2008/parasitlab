c84237ca791981a62e53010bd61769f00ebae1a2
Michel BOUY <michelange@wanadoo.fr>
2020-03-11 16:06:47 +0100

    ajout de url

--- resources/views/admin/detail/nomNum.blade.php
+++ resources/views/admin/detail/nomNum.blade.php
@@ -1,6 +1,6 @@
 {{-- BLOC VALABLE AUSSI BIEN POUR UNE VETERINAIRE QUE POUR UN ELEVEUR D'OU L'EMPLOI DE $personne --}}
 <div class="card-header alert-rouge-tres-fonce d-inline-flex">
-  <img class="img-40" src="{{ 'storage/img/icones/'.$personne->user->usertype->icone->nom}}" alt="{{ $personne->user->usertype->icone->nom }}">
+  <img class="img-40" src="{{ url('storage/img/icones/'.$personne->user->usertype->icone->nom)}}" alt="{{ $personne->user->usertype->icone->nom }}">
   <h5 class="card-title mx-3">{{ $personne->user->name }} </h5>
   <p class="text-truncate" data-toggle="tooltip" title="{{ $personne->num }}">( ede n° {{ $personne->num }} )</p>
 </div>
--- resources/views/admin/index/indexTitre.blade.php
+++ resources/views/admin/index/indexTitre.blade.php
@@ -1,6 +1,6 @@
 <div class="d-flex alert alert-bleu">
 
-  <img class="img-50" src="{{ 'storage/img/icones/'.$datas->icone }}" alt="{{ $datas->icone }}">
+  <img class="img-50" src="{{ url('storage/img/icones/'.$datas->icone) }}" alt="{{ $datas->icone }}">
 
   <h3 class="pt-3 ml-3">{{ ucfirst($datas->titre) ?? '' }}
     <small>
