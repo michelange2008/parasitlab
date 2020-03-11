1b37db25ac5ec43053380e4b2f6ad8a87a3c8c3e
Michel BOUY <michelange@wanadoo.fr>
2020-03-11 11:55:25 +0100

    remodif litjson

--- app/Http/Traits/LitJson.php
+++ app/Http/Traits/LitJson.php
@@ -6,19 +6,12 @@ trait LitJson {
     public function litJson($json)
     {
       $context = stream_context_create(
-        array(
-            "http" => array(
-                'method'=>"GET",
-                "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64)
-                            AppleWebKit/537.36 (KHTML, like Gecko)
-                            Chrome/50.0.2661.102 Safari/537.36\r\n" .
-                            "accept: text/html,application/xhtml+xml,application/xml;q=0.9,
-                            image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3\r\n" .
-                            "accept-language: es-ES,es;q=0.9,en;q=0.8,it;q=0.7\r\n" .
-                            "accept-encoding: gzip, deflate, br\r\n"
-            )
-        )
-    );
+          array(
+              "http" => array(
+                  "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+              )
+          )
+      );
 
       if(file_exists('storage/json/'.$json.'.json')) {
 
