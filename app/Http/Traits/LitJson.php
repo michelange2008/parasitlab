a53ffc57d90b382413a6a9cfda20186aba0af914
Michel BOUY <michelange@wanadoo.fr>
2020-03-11 12:10:13 +0100

    essai

--- app/Http/Traits/LitJson.php
+++ app/Http/Traits/LitJson.php
@@ -12,7 +12,7 @@ trait LitJson {
               )
           )
       );
-
+dd(asset('storage/json/'.$json.'.json'));
       if(file_exists('storage/json/'.$json.'.json')) {
 
         return $datas = json_decode(file_get_contents(asset('storage/json/'.$json.'.json'), false, $context));
