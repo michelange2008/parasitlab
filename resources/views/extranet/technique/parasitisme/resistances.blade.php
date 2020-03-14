8cc5a89b8297c422ee196f583358c5c8f1e400cb
Michel BOUY <michelange@wanadoo.fr>
2020-03-14 19:27:13 +0100

    moins HD

--- resources/views/extranet/technique/parasitisme/resistances.blade.php
+++ resources/views/extranet/technique/parasitisme/resistances.blade.php
@@ -23,7 +23,7 @@
 
         <div class="embed-responsive embed-responsive-16by9">
 
-          <iframe class="embed-responsive-item" src="{{ url('storage/img/resist/resistancesHD.mp4') }}" allowfullscreen></iframe>
+          <iframe class="embed-responsive-item" src="{{ url('storage/img/resist/resistances.mp4') }}" allowfullscreen></iframe>
 
         </div>
 
