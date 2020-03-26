b077611764cc2eaeb086bef1fc0a9c5f61539a03
Michel BOUY <michelange@wanadoo.fr>
2020-03-26 11:05:26 +0100

    corrections

--- resources/lang/fr/veterinaires.php
+++ resources/lang/fr/veterinaires.php
@@ -25,9 +25,9 @@ return [
   "resist" => "test de résistance vis-à-vis des anthelminthiques",
   "comment_faire" => "Comment faire ?",
 
-  "kit_envoi_1" => "Nous fournissons des kits d’envoi postal permettant un acheminement rapide et sécurisé des prélèvements.",
-  "kit_envoi_2" => "Ces kits contiennent une fiche de procédure de prélèvement, un formulaire de demande d'analyses, des sachets pour les prélèvements, des gants de prélèvement et une enveloppe pré-affranchie.",
-  "kit_envoi_3" => "N'hésitez pas à nous commander pour les proposer aux éleveurs à qui vous conseillez des réaliser des examens coproscopiques. Nous vous les envoyons gratuitement.",
+  "kit_envoi_1" => "Nous fournissons des kits d’envoi préaffranchis permettant un acheminement rapide et sécurisé des prélèvements.",
+  "kit_envoi_2" => "Chaque kit contient une fiche de procédure de prélèvement, un formulaire de demande d'analyses, des sachets et des gants de prélèvement et une enveloppe préaffranchie.",
+  "kit_envoi_3" => "N'hésitez pas à nous les commander pour les proposer aux éleveurs à qui vous conseillez de réaliser des examens coproscopiques. Nous vous les envoyons gratuitement.",
 
   "repondre_question_1" => "N'hésitez pas à nous contacter si vous avez des question sur les services que nous proposons et leurs modalités de mise en oeuvre.",
   "repondre_question_2" => "Nous pouvons vous apporter toutes les informations nécessaires sur les techniques utilisées au laboratoire.",
--- resources/views/extranet/commentfaireveto.blade.php
+++ resources/views/extranet/commentfaireveto.blade.php
@@ -4,11 +4,11 @@
 
     <h4>{{ __('veterinaires.comment_faire') }}</h4>
 
-    <p>{{ __('veterinaires.kit_envoi_1') }}.</p>
+    <p>{{ __('veterinaires.kit_envoi_1') }}</p>
 
-    <p>{{ __('veterinaires.kit_envoi_2') }}.</p>
+    <p>{{ __('veterinaires.kit_envoi_2') }}</p>
 
-    <p>{{ __('veterinaires.kit_envoi_3') }}.</p>
+    <p>{{ __('veterinaires.kit_envoi_3') }}</p>
 
   </div>
 
