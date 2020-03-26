e9138a1cc9d20b0580b163b9939edbf420b371a5
Michel BOUY <michelange@wanadoo.fr>
2020-03-26 11:49:24 +0100

    correction prélever

--- resources/views/extranet/analyses/enpratique/prelever/terre_lot.blade.php
+++ resources/views/extranet/analyses/enpratique/prelever/terre_lot.blade.php
@@ -6,15 +6,15 @@
 
     <div class="card-body">
 
-      <h4>Par terre ?</h4>
+      <h4>Comment prélever ?</h4>
       <p>Quelque soit l'analyse demandée, il faut prélever des crottes, crottins ou bouses.</p>
       <p>Il est possible de ramasser au sol quand on voit l'animal en train de faire: dans ce cas on est sûr de l'identité et de la fraîcheur.</p>
+      <p>Avec les ruminants, il est préférable de faire le prélèvement directement dans le rectum.</p>
       <p>Il ne faut en aucun cas ramasser des fèces par-terre si l'on ne sait pas quel animal les a fait et quand&nbsp!</p>
-      <p>Avec les ruminants et les porcs, il est préférable de faire le prélèvement directement dans le rectum.</p>
     </div>
 
     <div class="card-footer">
-      <p><i class="fas fa-exclamation-circle text-danger"></i> Une coproscopie se fait avec 4 grammes de crottes. Il n'est donc pas utile de nous envoyer de quantités importantes.</p>
+      <p><i class="fas fa-exclamation-circle text-danger"></i> Une coproscopie se fait avec une petite poignée de crottes. Il n'est donc pas utile de nous envoyer de quantités importantes.</p>
 
     </div>
 
@@ -25,7 +25,7 @@
     <img class="card-img-top"  src="storage/img/lot.jpg" alt="Brebis">
 
     <div class="card-body">
-      <h4>Par lots ?</h4>
+      <h4>Prélèvement individuel ou par lot ?</h4>
       <p>Dans un troupeau, les animaux ne sont pas tous parasités de la même façon.
         Quelques uns le sont beaucoup, d'autres pas du tout. C'est ce que l'on appelle <span class="lead">la surdispersion du parasitisme</span>
         (<a href="{{ route('parasitisme.surdispersion') }}" data-toggle="tooltip" title="La surdispersion du parasitisme"><abbr class="initialism"><i class="fas fa-book-reader"></i>&nbspen savoir plus</abbr></a>).
