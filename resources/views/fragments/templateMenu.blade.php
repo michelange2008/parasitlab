479cbcd76c55f0579506dfb7cb05634295d88f47
Michel BOUY <michelange@wanadoo.fr>
2020-04-02 17:45:18 +0200

    problème id menu

--- resources/views/fragments/templateMenu.blade.php
+++ resources/views/fragments/templateMenu.blade.php
@@ -4,13 +4,13 @@
     @foreach ($menu as $item)
       @if(@isset($item->sousmenu))
         <li class="nav-item nav-item-bleu-tres-clair dropdown">
-          <a id="{{ $item->id }}" class="nav-link dropdown-toggle" href="{{ route($item->route) }}" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <a id="{{ $item->id ?? '' }}" class="nav-link dropdown-toggle" href="{{ route($item->route) }}" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             {{ $item->nom }}
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             @foreach ($item->sousmenu as $sousmenu)
 
-              <a id="{{ $sousmenu->id }}" class="dropdown-item d-inline-flex justify-content-between" href="{{ route($sousmenu->route) }}">
+              <a id="{{ $sousmenu->id ?? '' }}" class="dropdown-item d-inline-flex justify-content-between" href="{{ route($sousmenu->route) }}">
                 {!! $sousmenu->nom !!}
               </a>
 
@@ -19,7 +19,7 @@
         </li>
       @else
         <li class="nav-item nav-item-bleu-tres-clair ">
-          <a id="{{ $item->id }}" class="nav-link" href="{{ route($item->route) }}">{!! $item->nom !!}</a>
+          <a id="{{ $item->id ?? '' }}" class="nav-link" href="{{ route($item->route) }}">{!! $item->nom !!}</a>
         </li>
       @endif
     @endforeach
