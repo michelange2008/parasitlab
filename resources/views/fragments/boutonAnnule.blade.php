49ae46e8ca3b95227aae6da8fb76680b3a7b33ab
michelange <michelange@wanadoo.fr>
2020-06-08 12:27:49 +0200

    Correction erreur blog

--- resources/views/fragments/boutonAnnule.blade.php
+++ resources/views/fragments/boutonAnnule.blade.php
@@ -1,3 +1,5 @@
-<a id="bouton_annule" class="my-3 ml-1" href="{{ $route ?? url()->previous() }}">
+<a id="bouton_annule" class="my-3 ml-1" href="{!! $route ?? url()->previous() !!}">
+
   <button type="button" class="btn btn-secondary" name="reset">@lang('boutons.annule')</button>
+
 </a>
