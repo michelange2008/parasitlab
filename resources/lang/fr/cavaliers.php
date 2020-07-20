5cc24960f90851f298ee91e6da5c0233516f1221
michelange2008 <michelange@wanadoo.fr>
2020-07-20 15:38:25 +0200

    correction cavaliers

--- resources/lang/fr/cavaliers.php
+++ resources/lang/fr/cavaliers.php
@@ -1,10 +1,10 @@
 <?php
 
 return array (
-  'bloc_1' => 
+  'bloc_1' =>
   array (
     'soustitre' => 'La parasitisme, un phénomène naturel moins inquiétant que l\'on ne croit',
-    'texte' => 
+    'texte' =>
     array (
       'texte_1' => 'Bien que les chevaux soient très souvent parasités, il est important de bien comprendre les différentes situations.',
       'texte_2' => 'Le poulain s\'infeste systématiquement par le lait de la mère mais aussi son crottin. Dans les premiers mois de sa vie, une attention particulière doit être attachée à la vermifugation du jeune sous la mère.',
@@ -13,29 +13,28 @@ return array (
     'alt' => 'Jument et poulain',
     'title' => 'Le poulain est très sensible au parasitisme, les adultes beaucoup moins',
   ),
-  'bloc_2' => 
+  'bloc_2' =>
   array (
     'soustitre' => 'Tenir compte des conditions de pâturage',
-    'texte' => 
+    'texte' =>
     array (
       'texte_1' => 'L\'intensité du parasitisme dépend surtout des condtions de pâturage.',
       'texte_2' => 'Des animaux nombreux sur de petites parcelles risquent de connaître de plus fortes infestations par les strongles digestifs.',
-      'texte_3' => 'Des animaux peu nombreux sur des parcelles assez grandes vont souvent être peu parasités.',
+      'texte_3' => 'Des animaux peu nombreux sur des parcelles assez grandes seront souvent être peu parasités.',
     ),
     'alt' => 'Chevaux au pâturage',
     'title' => 'L\'infestation parasitaire dépend beaucoup de la densité d\'animaux au pâturage',
   ),
-  'bloc_3' => 
+  'bloc_3' =>
   array (
     'soustitre' => 'Résistances aux antiparasitaires, ne gâchons pas nos dernières cartouches!',
-    'texte' => 
+    'texte' =>
     array (
       'texte_1' => 'La résistance vis-à-vis des anthelmintiques est régulièrement signalée pour les parasites digestifs des chevaux.
-                  Le recours à un petit nombre de molécules de façon systématique favorise à terme l\'apparition de ses résistances (cf. <a href=\'{{ route(\\\'parasitisme.fondamentaux\\\', 2) }}\'><i class=\'fas fa-video\'></i> vidéo</a>).
+                  Le recours à un petit nombre de molécules de façon systématique favorise à terme l\'apparition de ses résistances.
                   Il est indispensable d\'adopter aujourd\'hui une démarche visant à limiter l\'extension de ces résistances.',
-      'texte_2' => 'Pour cela, il suffit de ne traiter que les chevaux pour lesquels le parasitisme dépasse un certain seuil.
-                  C\'est en réalisant des examens coproscopiques que l\'on peut vérifier les niveaux d\'infestation par les strongles digestifs.',
-      'texte_3' => 'La réduction du nombre de traitements a aussi un effet positif, en diminuant les rejets d\'anthelminthiques dans l\'environnement (cf. <a href=\'{{ route(\\\'parasitisme.fondamentaux\\\', 1) }}\'><i class=\'fas fa-book-reader\'></i> article entomofaune</a>)',
+      'texte_2' => 'Pour cela, il suffit de ne traiter que les chevaux pour lesquels le parasitisme dépasse un certain niveau qui peut se vérifier par des examens coproscopiques.',
+      'texte_3' => 'La réduction du nombre de traitements a aussi un effet positif, en diminuant les rejets d\'anthelminthiques dans l\'environnement.',
     ),
     'alt' => 'Naseau de cheval',
     'title' => 'Il ne faut pas favoriser l\'apparition de résistances des strongles aux vermifuges',
