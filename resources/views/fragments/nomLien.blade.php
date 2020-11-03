327897464976fa7a63967c9ba0667a8a27d75e6e
Bouy Michel <michel.bouy@fibl.org>
2020-11-03 12:23:41 +0100

    bugfix: tooltip empêche de cliquer quand analyse est seule dans le tableau d'un éleveur

--- resources/views/fragments/nomLien.blade.php
+++ resources/views/fragments/nomLien.blade.php
@@ -2,7 +2,7 @@
 LA FICHE CORRESPONDANTE
 VARIABLES: id, route, nom -->
 <a href="{{ route($route, $id)}}"
-  data-toggle="tooltip" data-placement="top"
+  data-toggle="tooltip" data-placement="right"
   title="{{ __($tooltip ?? 'tooltips.affiche_infos') }}"
   class="">
   {{ $before ?? ''}}
