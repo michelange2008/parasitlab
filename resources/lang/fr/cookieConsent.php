3109768e40d3a0d6aa5cf2fcfb0a1757d2d2cbfe
michelange2008 <michelange@wanadoo.fr>
2020-11-20 15:33:14 +0100

    suppression phrase 2 heures dans cookieconsent

--- resources/lang/fr/cookieConsent.php
+++ resources/lang/fr/cookieConsent.php
@@ -2,7 +2,6 @@
 
 return array (
   'message' => 'Ce site nécessite l\'autorisation de cookies pour fonctionner correctement.
-	<br>C\'est cookies ont une durée de vie limitée à 2h et ne comportent pas de données personnelles.
 	<br>Ils servent à maintenir la connexion et renforcent le protection du site.
 	<br>N\'hésitez-pas à lire notre politique de protection des données personnelles dans la rubrique \'infos légales\'.<br>',
   'agree' => 'J\'ai compris et j\'accepte',
