7b2a2d1a68433dbc97eaacd45092a611a45ffced
michelange2008 <michelange@wanadoo.fr>
2021-03-08 06:04:31 +0100

    modif stockage doc

---
+++ app/Http/Controllers/DocController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * Controller destiné à présenter la documentation sur le site
+ *
+ * La document sur le site est issue principalement de phpDocumentor.
+ * On y a accès via le menu "A propos de"
+ */
+class DocController extends Controller
+{
+    //
+}
