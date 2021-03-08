820b53c4ad0fb5304e129def153dedd35deb6bf0
michelange2008 <michelange@wanadoo.fr>
2021-03-08 07:08:04 +0100

    Modification du menu labo avec création de Outils et Documentation

---
+++ resources/views/admin/doc.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('menu')
+
+  @include('labo.laboMenu')
+
+@endsection
+
+@section('content')
+
+  <div class="container-fluid">
+
+    <div class="row my-3 justify-content-center">
+
+coucou
+    </div>
+
+  </div>
+
+@endsection
