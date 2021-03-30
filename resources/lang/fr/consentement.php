fbb0390dcba881dc400abbb78422a8ae04375426
michelange2008 <michelange@wanadoo.fr>
2021-03-30 14:36:24 +0200

    modif consentement

--- resources/lang/fr/consentement.php
+++ resources/lang/fr/consentement.php
@@ -1,7 +1,7 @@
 <?php
 
 return array (
-  'suivre' => "Je suis d'accord pour être sollicité par le FiBL France dans le cadre de ses projets de recherche",
+  'suivre' => "Je suis d'accord pour être sollicité par le FiBL France dans le cadre de ses projets de recherche en lien notamment avec le parasitisme.",
   'recevoir' => "Je suis d'accord pour recevoir des informations du FiBL France",
   'consentement' => "Consentement",
   'rgpd' => "politique de protection des données personnelles",
