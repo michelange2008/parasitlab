21f5b39175d9bed84f9889c836855963d508ca01
michelange2008 <michelange@wanadoo.fr>
2021-10-08 11:08:05 +0200

    bugfix: pas d'affichage des noms de mélange à la saisie

--- resources/views/labo/resultats/resultatsSaisie.blade.php
+++ resources/views/labo/resultats/resultatsSaisie.blade.php
@@ -64,9 +64,15 @@
 
               <div class="card-header alert-bleu-tres-fonce d-flex justify-content-between">
 
-                {{-- Titre du prélèvement avec le numéro et le nom de l'animal --}}
-                <h5 class="">{{ ucfirst($prelevement->animal->nom ?? $prelevement->identification) }} {{ ucfirst($prelevement->animal->numero ?? '') }}</h5>
+                @if ($prelevement->estMelange)
 
+                  <h5> {{ ucfirst($prelevement->melange->nom ?? '') }}</h5>
+
+                @else
+                  {{-- Titre du prélèvement avec le numéro et le nom de l'animal --}}
+                  <h5 class="">{{ ucfirst($prelevement->animal->nom ?? $prelevement->identification) }} {{ ucfirst($prelevement->animal->numero ?? '') }}</h5>
+
+                @endif
               </div>
 
               <div class="card-body">
