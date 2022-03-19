72dc15147c4f57817f4fdad906dcee28997c5f9a
michelange2008 <michelange@wanadoo.fr>
2022-03-19 08:09:03 +0100

    documentation du js qui aide à créer es mélanges

--- resources/js/melangeAdd.js
+++ resources/js/melangeAdd.js
@@ -1,22 +1,24 @@
-// On inactive tous les champs qui le doivent
-$("#troupeau_animal_ou_melange_create").attr('disabled', true);
-var troupeau_id= 0;
+// Appelé dans la page labo.admin.melanges.choix_troupeau.blade
+$("#troupeau_animal_ou_melange_create").attr('disabled', true); // On inactive la liste des troupeaux
+$("#choix_troupeau").css("visibility", "hidden") // On rend invisible de bouton tant que l'éleveur et le troupeau ne sont pas choisis
+var troupeau_id= 0; // On met l'id du troupeau à 0
 $("#eleveur").on('change', function() {
   var eleveur_id = $('#eleveur option:selected').attr('value');
-  var url = $("#route").html().replace('user_id', eleveur_id);
-
+  var url = $("#route").html().replace('user_id', eleveur_id); // On modifie la ligne (invisible) avec le n° de l'éleveur
+  // On va chercher la liste des troupeaux de l'éleveur
   $.get({
     url: url,
   })
   .done(function(datas) {
     var troupeaux = JSON.parse(datas);
     var nb_troupeau = troupeaux.length;
-
+    // S'il n'y a pas de troupeau on prévient qu'il faut en créer un d'abord
     if (nb_troupeau == 0) {
       $("#troupeau_animal_ou_melange_create").empty().append('<option>Aucun troupeau: il faut en ajouter au moins un</option>').attr('disabled',true);
     }
+    // Sinon on fait une liste d'options avec une case vide au départ et la liste des troupeaux après
     else {
-      var options = '';
+      var options = '<option value="Choisir un troupeau" disabled>' + troupeau.nom + '</option>';
       for(var item in troupeaux){
         var troupeau = troupeaux[item];
         options += '<option value="' + troupeau.id + '">' + troupeau.nom + '</option>'
