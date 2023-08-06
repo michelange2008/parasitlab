cfeb7452bb525c6587c8d71d07d8f56201b6cba2
michelange2008 <michelange@wanadoo.fr>
2023-08-06 20:29:10 +0200

    modif résultats vétos

--- resources/views/labo/resultats/pdf/entetePdfVeto.blade.php
+++ resources/views/labo/resultats/pdf/entetePdfVeto.blade.php
@@ -1,57 +1,13 @@
-<table>
-  <tbody>
-    <tr>
-      <td style="width:380px"></td>
-      <td "style:border-top: solid 1px black; border-left: solid 1px black">
-        <p class="adresse"><strong>{{ $demande->veto->name }}</strong></p>
-      </td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>
-        <p class="adresse">{{ $demande->veto->veto->address_1 }}</p>
-      </td>
-    </tr>
-    <tr>
-      <td>
-      </td>
-      <td>
-        <p class="adresse">{{ $demande->veto->veto->address_2 }}</p>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <p class="adresse adresse-petit">@lang('communs.eleveur')&nbsp;: <strong>{{ $demande->user->name }}</strong></p>
-      </td>
-      <td>
-        <p class="adresse">{{ $demande->veto->veto->cp }} {{ $demande->user->commune }}</p>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <p class="adresse adresse-petit">{{ $demande->user->eleveur->address_1 }}</p>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <p class="adresse adresse-petit">{{ $demande->user->eleveur->address_2 }}</p>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <p class="adresse adresse-petit">{{ $demande->user->eleveur->cp }} {{ $demande->user->eleveur->commune }}</p>
-      </td>
-    </tr>
-  </tbody>
-</table>
 
 
-{{-- <div style="margin-left:380px; border: 1px solid black; padding-left:20px"> --}}
+        <p style="text-align:right" class="adresse"><strong>{{ $demande->tovetouser->name }}</strong></p>
+        <p style="text-align:right" class="adresse">{{ $demande->tovetouser->veto->address_1 }}</p>
+        <p style="text-align:right" class="adresse">{{ $demande->tovetouser->veto->address_2 }}</p>
+        <p style="text-align:right" class="adresse">{{ $demande->tovetouser->veto->cp }}
+            {{ $demande->tovetouser->veto->commune }}</p>
 
 
-{{-- </div>
-
-<div style="width:380px; border: 1px solid black; padding-left:20px">
-
-
-</div> --}}
+<p class="adresse adresse-petit">@lang('commun.eleveur')&nbsp;: <strong>{{ $demande->user->name }}</strong></p>
+<p class="adresse adresse-petit">{{ $demande->user->eleveur->address_1 }}</p>
+<p class="adresse adresse-petit">{{ $demande->user->eleveur->address_2 }}</p>
+<p class="adresse adresse-petit">{{ $demande->user->eleveur->cp }} {{ $demande->user->eleveur->commune }}</p>
--- resources/views/labo/resultats/pdf/enteteResultats.blade.php
+++ resources/views/labo/resultats/pdf/enteteResultats.blade.php
@@ -2,11 +2,11 @@
   <table class="" style="width:100%">
     <tr>
       <td>
-        <img width="250px" src="{{ url('storage/logo.svg') }}" alt="Logo du labo">
+        <img width="250px" src="images/logo.png" alt="Logo du labo">
         <p class="adresseFibl">{{ config('laboInfos.address_1') }}</p>
         <p class="adresseFibl">{{ config('laboInfos.address_2') }}</p>
       </td>
-      <td class="text-right">
+      <td style="text-align: right"">
         <p class="adresseFibl">@lang('demandes.signe_le') {{\Carbon\Carbon::parse($demande->date_signature)->isoFormat('DD/MM/Y')}}</p>
         <p class="adresseFibl">@lang('factures.ref'): {{$demande->id}}</p>
         <p class="adresseFibl">{{ucfirst($demande->anaacte->anatype->nom)}}</p>
