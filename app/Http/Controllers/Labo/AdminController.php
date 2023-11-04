02c270b837b3612cb1d3987d1b9b72183f97e322
michelange2008 <michelange@wanadoo.fr>
2023-11-04 19:12:09 +0100

    suite du dashboard: partie centrale

--- app/Http/Controllers/Labo/AdminController.php
+++ app/Http/Controllers/Labo/AdminController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\Labo;
 
 use App\Http\Controllers\Controller;
 use App\Http\Traits\LitJson;
+use App\Models\Productions\Demande;
+use App\Models\Productions\Facture;
+use App\Models\Productions\Prelevement;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 /**
  * Affichage du tableau de bord des utilisateurs Labo
@@ -19,8 +23,24 @@ class AdminController extends Controller
      **/
     public function index()
     {
+        $demandes_en_cours = Demande::where('acheve', 0)->orderBy('date_prelevement', 'DESC')->get();
+        $prelevements = Prelevement::all();
+        $factures_a_etablir = DB::table('demandes')->where('facturee', 0)
+                                ->join('users', 'users.id', 'demandes.user_id')
+                                ->join('anaactes', 'anaactes.id', 'demandes.anaacte_id')
+                                ->join('tvas', 'tvas.id', 'anaactes.tva_id')
+                                ->select('users.name as user_name', 'users.id as user_id', 'anaactes.nom as anaacte_nom', 
+                                'anaactes.pu_ht as pu_ht', 'tvas.taux as tva')
+                                ->get();
+        $factures_a_etablir = $factures_a_etablir->groupBy('user_name');
+        $factures_dues = Facture::where('payee', 0)->orderBy('faite_date', 'DESC')->get();
+
         return view('admin.dashboard', [
             'menu' => $this->litJson('menuLabo'),
+            'demandes_en_cours' => $demandes_en_cours,
+            'factures_a_etablir' => $factures_a_etablir,
+            'factures_dues' => $factures_dues,
+            'prelevements' => $prelevements,
         ]);
     }
 }
--- resources/views/admin/dashboard/dashmain.blade.php
+++ resources/views/admin/dashboard/dashmain.blade.php
@@ -1,17 +1,97 @@
-<div class="col p-2">
+<div class="row my-3">
 
-    @bouton([
-        'type' => 'route',
-        'route' => 'demandes.create',
-        'intitule' => 'Nouvelle demande d\'analyse',
-        'fa' => 'fas fa-microscope',
-    ])
+    <div class="col">
 
-    @bouton([
-        'type' => 'route',
-        'route' => 'user.create',
-        'intitule' => 'Nouvel utilisateur',
-        'fa' => 'fas fa-user-plus',
-    ])
+        @bouton([
+            'type' => 'route',
+            'route' => 'demandes.create',
+            'intitule' => 'Nouvelle demande d\'analyse',
+            'fa' => 'fas fa-microscope',
+        ])
+
+        @bouton([
+            'type' => 'route',
+            'route' => 'user.create',
+            'intitule' => 'Nouvel utilisateur',
+            'fa' => 'fas fa-user-plus',
+        ])
+
+    </div>
+</div>
+
+<div class="row my-4">
+
+    <div class="col-lg-4">
+        <div class="d-flex align-items-center bg-bleu-tres-clair p-2">
+            <img class="img-40" src="{{ url('storage/img/icones/analyse.svg') }}" alt="">
+            <h4 class="h4 mb-0 pl-1">Analyses en cours</h4>
+        </div>
+        <div>
+            @foreach ($demandes_en_cours as $demande)
+                <div class="d-flex p-3 border">
+                    <div>
+                        <img class="img-40" src="{{ url('storage/img/icones/' . $demande->espece->icone->nom) }}"
+                            alt="">
+                    </div>
+                    <div class="pl-2">
+                        <p class="mb-0">{{ $demande->user->name }}</p>
+                        <p class="mb-0 color-bleu">
+                            <a href="{{ route('demandes.show', $demande->id) }}">
+                                {{ ucfirst($demande->anaacte->anatype->nom) }}
+                                ({{ $prelevements->where('demande_id', $demande->id)->count() }} prlt.s)
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+    <div class="col-lg-4">
+        <div class="d-flex align-items-center bg-bleu-clair text-white p-2">
+            <img class="img-40" src="{{ url('storage/img/icones/factures_clair.svg') }}" alt="">
+            <h4 class="h4 mb-0 pl-1">Factures à établir</h4>
+        </div>
+        <div>
+            @foreach ($factures_a_etablir as $user_name => $demandes)
+                <div class="d-flex p-3 border">
+                    <div class="pl-2">
+                        <p class="mb-1 color-bleu">
+                            <a href="{{ route('factures.createFromUser', $demandes[0]->user_id) }}">
+                                {{ $user_name }}
+                            </a>
+                        </p>
+                        @foreach ($demandes as $demande)
+                            <p class="mb-0">
+                                {{ ucfirst($demande->anaacte_nom) }}
+                                {{ number_format(round($demande->pu_ht * (1 + $demande->tva), 2), 2, ',', ' ') }} €
+                            </p>
+                        @endforeach
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+    <div class="col-lg-4">
+        <div class="d-flex align-items-center bg-rouge-tres-clair p-2">
+            <img class="img-40" src="{{ url('storage/img/icones/cash.svg') }}" alt="">
+            <h4 class="h4 mb-0 pl-1">Factures dues</h4>
+        </div>
+        <div>
+            @foreach ($factures_dues as $facture)
+                <div class="d-flex p-3 border">
+                    <div class="pl-2">
+                        <p class="mb-1">
+                            {{ $user_name }}
+                            <a href="{{ route('factures.show', $facture->id) }}">
+                                <span class="color-bleu">n°{{ $facture->id}}</span>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 
 </div>
