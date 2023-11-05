5b4d58412667f1171a6821ed48023ccd3c7ef4d9
michelange2008 <michelange@wanadoo.fr>
2023-11-05 08:41:11 +0100

    modification apparence globale

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -12,7 +12,7 @@
 
     <div class="row justify-content-center">
 
-      <div class="p-2 col text-center bg-light border-bottom">
+      <div class="p-2 col text-center bg-light">
 
         <h1 class="h2">~ Tableau de bord ~</h1>
       </div>
@@ -21,19 +21,19 @@
 
     <div class="row">
 
-        <div class="d-none d-md-block col-md-2 bg-secondary">
+        <div class="d-none d-md-block col-md-2 bg-light">
 
             @include('admin.dashboard.dashmenu')
 
         </div>
 
-        <div class="col-lg-8 col-md-10 border-left border-right">
+        <div class="col-lg-8 col-md-10 border-left border-right border-top">
 
             @include('admin.dashboard.dashmain')
 
         </div>
 
-        <div class="d-none d-lg-block col-lg-2 bg-secondary">
+        <div class="d-none d-lg-block col-lg-2 bg-light">
 
             @include('admin.dashboard.dashstats')
 
