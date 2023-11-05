39ab8c069677fcc4723e68b2fc72d974eff8e103
michelange2008 <michelange@wanadoo.fr>
2023-11-05 18:48:26 +0100

    finalisation dashboard

--- app/Http/Controllers/Labo/AdminController.php
+++ app/Http/Controllers/Labo/AdminController.php
@@ -25,8 +25,13 @@ class AdminController extends Controller
     public function index()
     {
         $demandes_en_cours = Demande::where('acheve', 0)->orderBy('date_prelevement', 'DESC')->get();
+        $demandes_non_signees = Demande::where('acheve', 1)->where('signe', 0)
+                                ->orderBy('date_prelevement', 'DESC')->get();
+        $demandes_non_envoyees = Demande::where('acheve', 1)->where('signe', 1)
+                                ->where('envoye', 0)->orderBy('date_prelevement', 'DESC')->get();
         $prelevements = Prelevement::all();
-        $factures_a_etablir = DB::table('demandes')->where('facturee', 0)
+        $factures_a_etablir = DB::table('demandes')->where('acheve', 1)->where('signe', 1)
+                                ->where('envoye', 1)->where('facturee', 0)
                                 ->join('users', 'users.id', 'demandes.user_id')
                                 ->join('anaactes', 'anaactes.id', 'demandes.anaacte_id')
                                 ->join('tvas', 'tvas.id', 'anaactes.tva_id')
@@ -45,9 +50,11 @@ class AdminController extends Controller
         return view('admin.dashboard', [
             'menu' => $this->litJson('menuLabo'),
             'demandes_en_cours' => $demandes_en_cours,
+            'demandes_non_signees' => $demandes_non_signees,
+            'demandes_non_envoyees' => $demandes_non_envoyees,
             'factures_a_etablir' => $factures_a_etablir,
             'factures_dues' => $factures_dues,
-            'prelevements' => $prelevements,
+            'prelevements' => $prelevements, // Destiné à compter le nombre de prélèvements pour une analyse en cours
         ]);
     }
 }
--- resources/views/admin/dashboard/dashmain.blade.php
+++ resources/views/admin/dashboard/dashmain.blade.php
@@ -21,86 +21,91 @@
 
 <div class="row my-4">
 
-    <div class="col-lg-4">
-        <div class="d-flex align-items-center bg-bleu-tres-clair p-2">
-            <img class="img-40" src="{{ url('storage/img/icones/analyse.svg') }}" alt="">
-            <h4 class="h4 mb-0 pl-1">Analyses en cours</h4>
-        </div>
-        <table class="table table-hover table-bordered">
-            @foreach ($demandes_en_cours as $demande)
-                <tr>
-                    <td role="button">
-                        <a href="{{ route('demandes.show', $demande->id) }}">
-                            <div class="d-flex">
-                                <div>
-                                    <img class="img-40"
-                                        src="{{ url('storage/img/icones/' . $demande->espece->icone->nom) }}"
-                                        alt="">
-                                </div>
-                                <div class="pl-2">
-                                    <p class="mb-0">{{ $demande->user->name }}</p>
-                                    <p class="mb-0 color-bleu">
-                                        {{ ucfirst($demande->anaacte->anatype->nom) }}
-                                        <span title="Nombre de prélèvements">
-                                            ({{ $prelevements->where('demande_id', $demande->id)->count() }})
-                                        </span>
+    <div class="col-md-6">
+
+        @include('admin.dashboard.dashmain.dashdemandes', [
+            'icone' => 'analyse.svg',
+            'titre' => 'analyses_en_cours',
+            'aucune' => '0_demande_en_cours',
+            'demandes' => $demandes_en_cours,
+        ])
+
+        @include('admin.dashboard.dashmain.dashdemandes', [
+            'icone' => 'signature.svg',
+            'titre' => 'analyses_a_signer',
+            'aucune' => '0_analyses_non_signes',
+            'demandes' => $demandes_non_signees,
+        ])
+
+        @include('admin.dashboard.dashmain.dashdemandes', [
+            'icone' => 'envoye.svg',
+            'titre' => 'analyses_a_envoyer',
+            'aucune' => '0_analyses_non_envoyees',
+            'demandes' => $demandes_non_envoyees,
+        ])
+
+    </div>
+
+    <div class="col-md-6">
+        <div>
+
+            <div class="d-flex align-items-center bg-bleu-clair text-white p-2">
+                <img class="img-40" src="{{ url('storage/img/icones/factures_clair.svg') }}" alt="">
+                <h4 class="h4 mb-0 pl-1">@lang('dashboard.factures_a_etablir')</h4>
+            </div>
+            @if ($factures_a_etablir->count() == 0)
+                <p class="p-3 font-italic text-secondary">@lang('dashboard.0_facture_a_etablir')</p>
+            @else
+                <table class="table table-hover">
+                    @foreach ($factures_a_etablir as $user_name => $demandes)
+                        <tr class="border" role="button">
+                            <td class="pl-2">
+                                <a href="{{ route('factures.createFromUser', $demandes[0]->user_id) }}">
+                                    <p class="mb-1">
+                                        {{ $user_name }}
                                     </p>
-                                </div>
-                            </div>
-                        </a>
-                    </td>
-                </tr>
-            @endforeach
-        </table>
-    </div>
-
-    <div class="col-lg-4">
-        <div class="d-flex align-items-center bg-bleu-clair text-white p-2">
-            <img class="img-40" src="{{ url('storage/img/icones/factures_clair.svg') }}" alt="">
-            <h4 class="h4 mb-0 pl-1">Factures à établir</h4>
+                                    @foreach ($demandes as $demande)
+                                        <p class="mb-0 color-bleu">
+                                            {{ ucfirst($demande->anaacte_nom) }}
+                                            {{ number_format(round($demande->pu_ht * (1 + $demande->tva), 2), 2, ',', ' ') }}
+                                            €
+                                        </p>
+                                    @endforeach
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </table>
+            @endif
         </div>
-        <table class="table table-hover">
-            @foreach ($factures_a_etablir as $user_name => $demandes)
-                <tr class="border" role="button">
-                    <td class="pl-2">
-                        <a href="{{ route('factures.createFromUser', $demandes[0]->user_id) }}">
-                            <p class="mb-1">
-                                {{ $user_name }}
-                            </p>
-                            @foreach ($demandes as $demande)
-                                <p class="mb-0 color-bleu">
-                                    {{ ucfirst($demande->anaacte_nom) }}
-                                    {{ number_format(round($demande->pu_ht * (1 + $demande->tva), 2), 2, ',', ' ') }} €
-                                </p>
-                            @endforeach
-                        </a>
-                    </td>
-                </tr>
-            @endforeach
-        </table>
-    </div>
 
-    <div class="col-lg-4">
-        <div class="d-flex align-items-center bg-rouge-tres-clair p-2">
-            <img class="img-40" src="{{ url('storage/img/icones/cash.svg') }}" alt="">
-            <h4 class="h4 mb-0 pl-1">Factures dues</h4>
+        <div class="">
+
+            <div class="d-flex align-items-center bg-rouge-tres-clair p-2">
+                <img class="img-40" src="{{ url('storage/img/icones/cash.svg') }}" alt="">
+                <h4 class="h4 mb-0 pl-1">@lang('dashboard.factures_dues')</h4>
+            </div>
+            @if ($factures_dues->count() == 0)
+                <p class="p-3 font-italic text-secondary">@lang('dashboard.0_facture_due')</p>
+            @else
+                <table class="table table-hover table-bordered">
+                    @foreach ($factures_dues as $facture)
+                        <tr class="" role="button">
+                            <td class="pl-2">
+                                <a href="{{ route('factures.show', $facture->id) }}">
+                                    <p class="mb-1">{{ $facture->user->name }}</p>
+
+                                    <p class="color-bleu">
+                                        facture n°{{ $facture->id }}:
+                                        <span class="font-weight-bold">{{ $facture->total_ttc }}</span>
+                                    </p>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </table>
+            @endif
         </div>
-        <table class="table table-hover table-bordered">
-            @foreach ($factures_dues as $facture)
-                <tr class="" role="button">
-                    <td class="pl-2">
-                        <a href="{{ route('factures.show', $facture->id) }}">
-                            <p class="mb-1">{{ $user_name }}</p>
-
-                            <p class="color-bleu">
-                                facture n°{{ $facture->id }}:
-                                <span class="font-weight-bold">{{ $facture->total_ttc }}</span>
-                            </p>
-                        </a>
-                    </td>
-                </tr>
-            @endforeach
-        </table>
     </div>
 
 </div>
